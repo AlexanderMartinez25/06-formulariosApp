8d11bf97568df036c2f5e81f3836f7f03479d674
Alexander <alexander.martinez@lapatota.cl>
2022-07-07 22:30:45 -0400

    Submit de formulario

--- src/app/reactive/basicos/basicos.component.ts
+++ src/app/reactive/basicos/basicos.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
@@ -7,7 +7,7 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
   styles: [
   ]
 })
-export class BasicosComponent {
+export class BasicosComponent implements OnInit {
 
   // miFormulario: FormGroup = new FormGroup({
   //   nombre: new FormControl('RTC 4080TI'),
@@ -22,8 +22,32 @@ export class BasicosComponent {
   })
   constructor(private fb: FormBuilder) { }
 
+  ngOnInit(): void {
+    // le asignamos un valor al formulario
+    this.miFormulario.reset({
+      nombre: 'RTX 4080TI',
+      precio: 1600,
+      existencias: 2
+    })
+  }
+
+  // validamos campos
   campoEsValido(campo: string) {
     return this.miFormulario.controls[campo].errors
       && this.miFormulario.controls[campo].touched;
+  }
+
+  guardar() {
+    // marcar todos los campos como tocados
+    // para que se activen todos los errores al guardar
+    if (this.miFormulario.invalid) {
+      this.miFormulario.markAllAsTouched();
+      return;
+    }
+
+    console.log(this.miFormulario.value);
+    // limpiar form
+    this.miFormulario.reset();
+
   }
 }
