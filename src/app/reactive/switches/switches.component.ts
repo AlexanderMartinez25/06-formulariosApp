f2e265b34fe2df1d9bbcda3365b4ed95a00b63df
Alexander <alexander.martinez@lapatota.cl>
2022-07-09 22:48:36 -0400

    Form Reactivos Switches

--- src/app/reactive/switches/switches.component.ts
+++ src/app/reactive/switches/switches.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-switches',
@@ -8,9 +9,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SwitchesComponent implements OnInit {
 
-  constructor() { }
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
+    // tambien se puede con setValue pero dará error 
+    // por no tener las condiciones en el obj persona
+    this.miFormulario.reset({
+      ...this.persona,
+      // utilizamos el spread operator para mandar las ondiciones tambien
+      condiciones: false
+    })
+  }
+
+  miFormulario: FormGroup = this.fb.group({
+    genero: ['M', Validators.required],
+    notificaciones: [true, Validators.required],
+    // para que el formmulario funciones solo cuando el boolena sea true
+    condiciones: [false, Validators.requiredTrue]
+  })
+
+  persona = {
+    genero: 'F',
+    notificaciones: true,
   }
 
 }
